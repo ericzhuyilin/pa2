6c9f3528dbfc149dbb0cb3041c5bbedcf7c5f0c3
ericzhuyilin <yiz095@ucsd.edu>
2022-08-28 17:08:17 -0700

    updates

--- test.s
+++ test.s
@@ -1,25 +1,21 @@
-@ Describe the hardware to the assembler1~
       .arch   armv6
       .cpu    cortex-a53
       .syntax unified
 
-@ Declare any external functions
       .extern putchar
       .extern printf
       .global main
 
-@ Define any constants
       .equ    FP_OFFSET, 4
       .equ    LC_A, 0x61
       .equ    LC_Z, 0x7a
       .equ    NULL, 0x0
       .equ    EXIT_SUCCESS, 0
-
-@ Start data segment where you will define your string
+      
       .data
 mesg: .asciz "Hello World!"
+//prstr: .asciz "upper case of the string = %s\n"
 
-@ Start text segment and begin writing your function
       .text
       .type   main, %function
 main: push  {fp, lr}
@@ -31,21 +27,24 @@ main: push  {fp, lr}
       beq   done
 loop:
       cmp r0, LC_A
-      blt next
-      cmp r0, LC_Z
+      blt next 
+      cmp r0, LC_Z 
       bgt next
-      sub r0, 32
+      sub r0, 32 
 
-next:
+next: 
       bl putchar
       add r4, r4, 1
       ldrb r0, [r4]
       cmp r0, NULL
       bne loop
-
+      
 done:
       mov r0, EXIT_SUCCESS
       sub sp, fp, FP_OFFSET
       pop {fp, lr}
+     // ldr r0, =prstr
+     // ldr r4, =mesg
+     // bl  printf
       bx  lr
 .end
